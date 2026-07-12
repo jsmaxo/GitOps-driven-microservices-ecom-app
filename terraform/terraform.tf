45951a4776ed1ebc019b914ebc50f14f6422ced7
jsmaxo <nanayakkaraj210@gmail.com>
2026-07-12 12:17:19 +0530

    added backend files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "s3" {
+    bucket = "terraform-backend-bucket-023703348895"
+    key    = "s3-backend"
+    region = "eu-west-1"
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
